1cf255663f6e0a9a19e9e421177223386a6cc93a
Jason Crawford <daxapps777@gmail.com>
2017-07-09 17:55:47 -0500

    Add reset.css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+import './reset.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './reset.css';
 import './index.css';
 
 import Game from './components/game';
 
 ReactDOM.render(
-	<Game />, 
-	document.getElementById('root')
+    <Game />,
+    document.getElementById('root')
 );
+
 
